bdedd09ada183a7d1398b7cbb93ca049facbecc1
Zetton66 <irsyadmuhammad46@gmail.com>
2025-11-18 20:51:12 +0700

    Update switch.5.js

--- switch.5.js
+++ switch.5.js
@@ -1,23 +1,25 @@
-let angka1 = 17;
+let angka1 = 15;
 let angka2 = 4;
-let operator = "%";
+let operator = "%"; 
+let hasil;
 
 switch (operator) {
-  case "+":
-    console.log("Hasil: " + (angka1 + angka2));
-    break;
-  case "-":
-    console.log("Hasil: " + (angka1 - angka2));
-    break;
-  case "*":
-    console.log("Hasil: " + (angka1 * angka2));
-    break;
-  case "/":
-    console.log("Hasil: " + (angka1 / angka2));
-    break;
-  case "%":
-    console.log("Hasil: " + (angka1 % angka2));
-    break;
-  default:
-    console.log("Operator tidak valid! Gunakan +, -, *, /, atau %.");
+    case '+':
+        hasil = angka1 + angka2;
+        break;
+    case '-':
+        hasil = angka1 - angka2;
+        break;
+    case '*':
+        hasil = angka1 * angka2;
+        break;
+    case '/':
+        hasil = angka1 / angka2;
+        break;
+    case '%':
+        hasil = angka1 % angka2;
+        break;
+    default:
+        hasil = "Error: Operator tidak valid. Gunakan (+, -, *, /, atau %)";
 }
+console.log(`${angka1} ${operator} ${angka2} = ${hasil}`);
